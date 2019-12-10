1c860ae37e25e28abd469ec1699b538bd031d5b6
zjk0803 <zjksimple@163.com>
2019-12-10 20:54:35 +0800

    1

--- jd/spiders/jd_spider.py
+++ jd/spiders/jd_spider.py
@@ -2,12 +2,17 @@
 import scrapy
 from  ..items import JdItem
 import sys
-
+from scrapy.selector import Selector
 class JdSpiderSpider(scrapy.Spider):
     name = "jd_spider"
     allowed_domains = ["www.jd.com"]
     start_urls = ['http://www.jd.com/']
 
     def parse(self, response):
-
+        selector = Selector(response)
+        lis = selector.xpath('//ul[class="gl-warp clearfix"]/li')
+        for li in lis:
+            item = JdItem()
+            try:
+                item['name'] = li.xpath('div/div[class="p-name p-name-type-2"]/text()').ex
         pass
